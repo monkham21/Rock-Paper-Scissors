773896c24e6438fd8eb20c4c8febe86acc915545
Nang <6531501222@lamduan.mfu.ac.th>
2022-09-05 11:54:48 +0700

    Add buttons and logic

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Rock Paper Scissors game that can be played in the console. 
 The player plays against the computer.
+
+Project Repo:
+https://github.com/monkham21/Rock-Paper-Scissors
--- game.js
+++ game.js
@@ -12,50 +12,51 @@ function getComputerChoice() {
 // single round game
 function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase();
-    // player wins
     if ((playerSelection == "rock" && computerSelection == "scissor" ) || (playerSelection == "paper" && computerSelection == "rock" ) || (playerSelection == "scissor" && computerSelection == "paper" )) {
-        return `You Win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}.`;
-    }
-    // draw
-    else if (playerSelection == computerSelection) {
-        return "It's a draw."
-    }
-    // computer wins
-    else {
-        return `You Lose! ${computerSelection[0].toUpperCase() + computerSelection.substring(1)} beats ${playerSelection}.`;
+        return 1  // player wins
+    } else if (playerSelection == computerSelection) {
+        return 0 // draw
+    } else {
+        return -1 // computer wins
     }
 }
 
 
-// five rounds game
 function game(){
     let player = 0, computer = 0;
 
-    // 5 rounds
-    for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("Rock,Paper,Scissor: ");
-        const computerSelection = getComputerChoice();
+    // player select through buttons
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelectorAll('button').forEach(function(button) {
+            button.onclick = function() {
+                const playerSelection = button.dataset.choice;
+                const computerSelection = getComputerChoice();
+                console.log(`Player: ${playerSelection}\nComputer : ${computerSelection}`)
+                const round = playRound(playerSelection, computerSelection);
 
-        // print choices
-        console.log(`Player: ${playerSelection}\nComputer : ${computerSelection}`)
+                if (round == 1) {
+                        player++;
+                    } else if (round == -1) {
+                        computer++;
+                    }
+                console.log(player, computer);
+            }
+        });
+    });
 
-        const round = playRound(playerSelection, computerSelection);
-        if (round.search("Win") != -1) {
-            player++;
-        } else if (round.search("Lose") != -1) {
-            computer++;
-        }
-    }
+
+
+
 
     // determine winner
-    if (player > computer) {
-        console.log("You Win!");
-    } else if (computer > player) {
-        console.log("Computer Wins!");
-    } else {
-        console.log("It's a draw.");
-    }
-    console.log(`You ${player} : Computer ${computer}`);
+//    if (player > computer) {
+//        console.log("You Win!");
+//    } else if (computer > player) {
+//        console.log("Computer Wins!");
+//    } else {
+//        console.log("It's a draw.");
+//    }
+//    console.log(`You ${player} : Computer ${computer}`);
 }
 
 game();
